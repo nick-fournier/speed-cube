052505f680e77b140a57b55e88caf979f31d53e8
nick-fournier <nichfournier@gmail.com>
2025-04-19 16:51:03 -0700

    Buggy working implementation of manufacturer's code

--- speed-cube.cpp
+++ speed-cube.cpp
@@ -1,17 +1,27 @@
 #include <stdio.h>
+#include <cmath>
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
-#include "LCD.h"
+#include "hardware/watchdog.h"
 #include "L76B.h"
-#include <cmath>
+extern "C" {
+    #include "DEV_Config.h"
+    #include "LCD_Driver.h"
+    #include "LCD_Touch.h"
+    #include "LCD_GUI.h"
+    #include "LCD_Bmp.h"
+}
 
 
 L76B l76b;
-LCD lcd;
+// LCD lcd;
 
 // Core 1 function: GPS processing
 void core1_main() {
     // Initialize UART and interrupts on Core 1
+
+    printf("Starting GPS on Core 1 with UART interrupts...\n");
+
     l76b.init();
 
     while (true) {
@@ -19,31 +29,33 @@ void core1_main() {
     }
 }
 
-
+// Core 0 function: Main program
 int main() {
-    // Initialize standard library
-    stdio_init_all();
-    printf("Starting GPS on Core 1 with UART interrupts...\n");
+    // Initialize peripherals
+    System_Init();
 
+    // Initialize L76B GPS module
     multicore_launch_core1(core1_main);
 
-    // Initialize the LCD
-    lcd.init();
-    lcd.setRotation(LCD_ROTATION_180);
-    lcd.setContrast(0x40);  // Lower contrast
-    lcd.clear(COLOR_BLACK);
-    lcd.drawString(20, 20, "SOG", COLOR_WHITE, COLOR_BLACK, 2);
-    lcd.drawString(120, 30, "kts", COLOR_WHITE, COLOR_BLACK, 2);
+    // Initialize SD card
+    SD_Init();
 
-    lcd.drawString(20, 120, "Heading", COLOR_WHITE, COLOR_BLACK, 2);
+    // Initialize LCD
+    LCD_SCAN_DIR lcd_scan_dir = SCAN_DIR_DFT;
+    LCD_Init(lcd_scan_dir, 800);
+    GUI_Clear(LCD_BACKGROUND);
+
+
     
-    // Horizontal line
-    lcd.drawLine(0, 200, 320, 200, COLOR_WHITE);
+    GUI_DisString_EN(20, 20, "SOG", &Font24, LCD_BACKGROUND, WHITE);
+    GUI_DisString_EN(120, 30, "kts", &Font24, LCD_BACKGROUND, WHITE);
+    GUI_DisString_EN(20, 120, "Heading", &Font24, LCD_BACKGROUND, WHITE);
+    GUI_DrawLine(0, 200, 320, 200, WHITE, LINE_SOLID, DOT_PIXEL_1X1);
+    
 
-    lcd.drawString(20, 260, "VMG", COLOR_WHITE, COLOR_BLACK, 2);
-    lcd.drawString(120, 280, "kts", COLOR_WHITE, COLOR_BLACK, 2);
-
-    lcd.drawString(20, 360, "Wind Angle", COLOR_WHITE, COLOR_BLACK, 2);
+    GUI_DisString_EN(20, 260, "VMG", &Font24, LCD_BACKGROUND, WHITE);
+    GUI_DisString_EN(120, 280, "kts", &Font24, LCD_BACKGROUND, WHITE);
+    GUI_DisString_EN(20, 360, "Wind Angle", &Font24, LCD_BACKGROUND, WHITE);
 
     while (true) {
         L76B::GPSData data = l76b.getData();
@@ -64,18 +76,21 @@ int main() {
             (int)(fmod((data.time / 100), 100)),
             (int)(fmod(data.time, 100))
         );
-        lcd.drawString(220, 0, timestamp, COLOR_WHITE, COLOR_BLACK, 2);
+
+        GUI_DisString_EN(180, 0, timestamp, &Font24, LCD_BACKGROUND, WHITE);
+        // lcd.drawString(220, 0, timestamp, COLOR_WHITE, COLOR_BLACK, 2);
 
         // Print speed under SOG
         char speedStr[20];
         snprintf(speedStr, sizeof(speedStr), "%.2f", data.speed);
-        lcd.drawString(20, 40, speedStr, COLOR_WHITE, COLOR_BLACK, 4);
+        GUI_DisString_EN(20, 40, speedStr, &Font24, LCD_BACKGROUND, WHITE);
+        // lcd.drawString(20, 40, speedStr, COLOR_WHITE, COLOR_BLACK, 4);
         
         // Print heading under speed
         char headingStr[20];
         snprintf(headingStr, sizeof(headingStr), "%.2f", data.heading);
-        lcd.drawString(20, 140, headingStr, COLOR_WHITE, COLOR_BLACK, 4);
-
+        GUI_DisString_EN(20, 140, headingStr, &Font24, LCD_BACKGROUND, WHITE);
+        // lcd.drawString(20, 140, headingStr, COLOR_WHITE, COLOR_BLACK, 4);
 
         sleep_ms(1000);
     }
